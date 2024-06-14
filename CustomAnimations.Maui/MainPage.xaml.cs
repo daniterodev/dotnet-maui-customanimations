4ad1eec9fb89a1757312c35a7b3107042b8c7a8c
Daniel Quintero Henao <danielquintero@technium.dev>
2024-06-13 23:55:19 -0500

    Project files

---
+++ CustomAnimations.Maui/MainPage.xaml.cs
@@ -0,0 +1,17 @@
+﻿namespace CustomAnimations.Maui
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            var animation = new Animation(v => image.TranslationY = v, -250, 0);
+            animation.Commit(image, "Animation",8, 1000, Easing.SinIn);
+        }
+    }
+
+}
